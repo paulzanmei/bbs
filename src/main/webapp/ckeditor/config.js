a91434482896bf26959e89a9854cd8646bb108f8
zhongbaoluo <zhongbaoluo@SH-HK-ZHONGBL.vcredit.cn>
2016-09-01 09:34:43 +0800

    修改图片上传

--- src/main/webapp/ckeditor/config.js
+++ src/main/webapp/ckeditor/config.js
@@ -9,9 +9,9 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.uiColor = '#AADC6E';
 	config.enterMode = CKEDITOR.ENTER_BR;
 	config.shiftEnterMode = CKEDITOR.ENTER_P;
-	config.filebrowserBrowseUrl = '/bbs/ckfinder';
-	config.filebrowserImageBrowseUrl = '/bbs/ckfinder?type=Images';
-    config.filebrowserFlashBrowseUrl = '/bbs/ckfinder?type=Flash';
+	config.filebrowserBrowseUrl = '/bbs/ckf';
+	config.filebrowserImageBrowseUrl = '/bbs/ckf?type=Images';
+    config.filebrowserFlashBrowseUrl = '/bbs/ckf?type=Flash';
     config.filebrowserUploadUrl = '/bbs/ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Files';
     config.filebrowserImageUploadUrl = '/bbs/ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Images';
     config.filebrowserFlashUploadUrl = '/bbs/ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Flash';
